746753141ed8161f8187a463001c764f27c19778
Antony Fernando <89105412+AntonyFernando3@users.noreply.github.com>
2026-01-29 17:04:08 -0300

    Delete Gerson directory

--- Gerson/exemplo14.cpp
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-
-int sequencia(int n){
-	if (h==1) return 3;
-	else return 3* sequencia(n-1)-1;
-}
--- Gerson/exemplo7.cpp
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-main() {
-	int *a, *b, c=5, d=10;
-	a=&c; b=&d;
-	printf("\n%d\t%d\n", *a, *b);
-	printf("\n%d\t%d\n", c,d);
-}
